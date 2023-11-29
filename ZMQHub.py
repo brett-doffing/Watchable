a8f2ad6d55e2a7c04dd7b432a07b729654348c50
doffing.brett <doffing.brett@gmail.com>
2023-11-29 17:52:29 -0600

    Comment out use of `CamerModel`

--- ZMQHub.py
+++ ZMQHub.py
@@ -1,5 +1,5 @@
 import imagezmq
-from Camera_Model import Camera_Model
+# from CameraModel import CameraModel
 import threading
 import multiprocessing
 from queue import Queue
@@ -8,7 +8,7 @@ from collections import OrderedDict
 class ZMQHub():
     def __init__(self, q):
         self.image_hub = imagezmq.ImageHub()
-        self.models = {}
+        # self.models = {}
         self.q = q
         self.thread = threading.Thread(target=self.start)
         self.thread.daemon = True
@@ -18,8 +18,8 @@ class ZMQHub():
         while True:
             host_name, image = self.image_hub.recv_image()
 
-            if self.models.get(host_name, None) is None:
-                self.models[host_name] = Camera_Model(host_name)
+            # if self.models.get(host_name, None) is None:
+            #     self.models[host_name] = CameraModel(host_name)
 
             self.q.put({'frame': image, 'name': host_name})
             self.image_hub.send_reply(b'OK')
