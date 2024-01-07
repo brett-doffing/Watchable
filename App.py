3608287f48a282b1d5632ff6942e9af6946ba7ab
doffing.brett <doffing.brett@gmail.com>
2024-01-07 08:57:11 -0600

    Add cameras and kill broadcasts on shutdown

--- App.py
+++ App.py
@@ -1,11 +1,23 @@
 from tkinter import *
 from CamsView import CamsView
+from Cameras.CameraMenu import CameraMenu
 
 if __name__ == "__main__":
     root = Tk()
     root.title('Watchful App')
     root.wm_geometry("960x640")
     app = CamsView(root)
+    broadcasts = []
+
+
+    def showCameraMenu():
+        CameraMenu(root, broadcasts)
+
+
+    def shutdown():
+        [process.kill() for process in broadcasts]
+        root.destroy()
+
     
     # create a menubar
     menubar = Menu(root)
@@ -13,6 +25,7 @@ if __name__ == "__main__":
     # create a menu
     file_menu = Menu(menubar)
     calibrate_menu = Menu(menubar)
+    camera_menu = Menu(menubar)
     # add the File menu to the menubar
     menubar.add_cascade(
         label="File",
@@ -22,14 +35,22 @@ if __name__ == "__main__":
         label="Calibration",
         menu=calibrate_menu
     )
+    menubar.add_cascade(
+        label="Cameras",
+        menu=camera_menu
+    )
     # add a menu item to the menu
     file_menu.add_command(
         label='Exit',
-        command=root.destroy
+        command=shutdown
     )
     calibrate_menu.add_command(
         label='Calibrate',
         command=app.calibrate,
+    )
+    camera_menu.add_command(
+        label='Add',
+        command=showCameraMenu,
     )
     
     root.mainloop()
